693dd174844a1b2a5d008bdc95fe74e414ba8758
Seraj Lee <Lucas_Naruto_AE@Yahoo.com>
2021-02-04 09:34:44 -0800

    Implemented full functionality base

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4,25 +4,119 @@ import flixel.FlxState;
 
 class PlayState extends FlxState
 {
+	public var player:Player;
+
+	var collider:FlxSpriteGroup;
+	var background:FlxSpriteGroup;
+
+	public var noah:NPC;
+	public var npcs:FlxTypedGroup<NPC>;
+
 	public var dialogueBox:DialogueBox;
+
+	public static var gameCamera:FlxCamera;
+
+	var uiCamera:FlxCamera;
 
 	override public function create()
 	{
 		super.create();
 
+		FlxG.camera.flash(FlxColor.BLACK, 4);
+
 		dialogueBox = new DialogueBox();
 		add(dialogueBox);
 
-		// dialogueBox.cameras = [uiCamera];
+		gameCamera = new FlxCamera(0, 0, FlxG.width, FlxG.height);
+		uiCamera = new FlxCamera(0, 0, FlxG.width, FlxG.height);
+
+		gameCamera.bgColor = 0xFF17142d;
+		uiCamera.bgColor = FlxColor.TRANSPARENT;
+
+		var project = new LdtkProject();
+
+		// Create a FlxGroup for all level layers
+		collider = new FlxSpriteGroup();
+
+		background = new FlxSpriteGroup();
+		add(background);
+
+		// Iterate all world levels
+		for (level in project.levels)
+		{
+			// Place it using level world coordinates (in pixels)
+			collider.setPosition(level.worldX, level.worldY);
+			background.setPosition(level.worldX, level.worldY);
+
+			createEntities(level.l_Entities);
+
+			// Attach level background image, if any
+			if (level.hasBgImage())
+				background.add(level.getBgSprite());
+
+			// Render layer "Background"
+			level.l_Background.render(background);
+
+			// Render layer "Collisions"
+			level.l_Collisions.render(collider);
+		}
+
+		for (tile in collider)
+		{
+			tile.immovable = true;
+		}
+		add(collider);
+
+		gameCamera.follow(player);
+		gameCamera.zoom = 2;
+
+		FlxG.cameras.reset(gameCamera);
+		FlxG.cameras.add(uiCamera);
+
+		FlxCamera.defaultCameras = [gameCamera];
+		// gameCamera.camera.setScrollBoundsRect(0, 0, background.width, background.height, true);
+		dialogueBox.cameras = [uiCamera];
 
 		dialogueBox.scrollFactor.set(0, 0);
 	}
 
+	function createEntities(entityLayer:LdtkProject.Layer_Entities)
+	{
+		var x:Int;
+		var y:Int;
+
+		x = entityLayer.all_Noah[0].pixelX;
+		y = entityLayer.all_Noah[0].pixelY;
+
+		npcs = new FlxTypedGroup<NPC>();
+
+		noah = new NPC(x, y);
+		noah.text = entityLayer.all_Noah[0].f_string;
+		noah.loadGraphic(AssetPaths.Noah__png, false, 32, 64);
+		trace(noah.width);
+		trace(noah.height);
+		noah.setFacingFlip(FlxObject.LEFT, true, false);
+		noah.setFacingFlip(FlxObject.RIGHT, false, false);
+
+		npcs.add(noah);
+		add(noah);
+
+		noah.facing = FlxObject.LEFT;
+
+		x = entityLayer.all_Player[0].pixelX;
+		y = entityLayer.all_Player[0].pixelY;
+
+		player = new Player(x, y);
+		add(player);
+	}
+
 	override public function update(elapsed:Float)
 	{
+		handleDialogBox();
+
 		super.update(elapsed);
 
-		handleDialogBox();
+		FlxG.collide(collider, player);
 	}
 
 	function handleDialogBox()
